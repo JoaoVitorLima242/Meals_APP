a5e907ce6650fbbc0f9af1a91afcb299e5b46eee
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-09 12:12:17 -0300

    make the category screen more clean

--- src/screens/Categories.js
+++ src/screens/Categories.js
@@ -2,18 +2,21 @@ import {
     FlatList,
 
 } from 'react-native';
-import CategoryGridTile from '../components/CategoryGridTile';
 
 // Components
-
+import CategoryGridTile from '../components/CategoryGridTile';
 // Services
 import { CATEGORIES } from '../services/data/dummy-data'
 
 const CategoriesScreen = () => {
+    const renderCategoryItem = (itemData) => {
+        return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
+    }
+
     return <FlatList
         data={CATEGORIES}
         keyExtractor={item => item.id}
-        renderItem={({item}) => <CategoryGridTile title={item.title} color={item.color}/> }
+        renderItem={renderCategoryItem}
     />
 }
 
