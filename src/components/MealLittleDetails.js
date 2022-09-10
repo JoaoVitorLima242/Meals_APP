6a88d97ef1512cb5e6af975350fb57d67a9335e2
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-09 21:44:20 -0300

    Meal little details component

---
+++ src/components/MealLittleDetails.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const MealLittleDetails = ({ duration, complexity, affordability }) => {
+
+    return (
+        <View style={styles.details}>
+            <Text style={styles.detailText}>{duration}min</Text>
+            <Text style={styles.detailText}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailText}>{affordability.toUpperCase()}</Text>
+        </View>
+    )
+}
+
+export default MealLittleDetails
+
+const styles = StyleSheet.create({
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    detailText: {
+        fontSize: 12,
+        margin: 10
+    },
+})
