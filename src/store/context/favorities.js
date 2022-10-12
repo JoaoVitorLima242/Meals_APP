d5397f4b3f91c0bc677abe887a74ba613fc21e5b
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-12 19:34:40 -0300

    passing values to provider

--- src/store/context/favorities.js
+++ src/store/context/favorities.js
@@ -1,14 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const FavoritesContext = createContext({
-    ids: [],
+    favoritiesMeals: [],
     addFavorite: (id) => {},
     removeFavorite: (id) => {},
 })
 
 const FavoritesProvider = ({children}) => {
+    const [favoritiesMeals, setFavoritesMeals] = useState([])
+
+    const addFavorite = (id) => {
+        setFavoritesMeals(state => [...state, id])    
+    }
+
+    const removeFavorite = (id) => {
+        setFavoritesMeals(state => state.filter(mealsId => mealsId !== id))    
+    }
+
+    const value = {
+        favoritiesMeals,
+        addFavorite,
+        removeFavorite
+    }
+
     return (
-        <FavoritesContext.Provider>
+        <FavoritesContext.Provider value={value}>
             {children}
         </FavoritesContext.Provider>
     )
